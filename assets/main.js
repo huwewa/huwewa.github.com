84ed70d0e82d67c5a4319766584511286b721069
huwewa <huwewa@gmail.com>
2020-04-03 14:52:17 +0800

    change quote

--- assets/main.js
+++ assets/main.js
@@ -171,7 +171,7 @@ xhrPosts.onreadystatechange = function () {
     randomQuotes(quoteData);
   }
 }
-// xhrPosts.send(null);
+xhrPosts.send(null);
 //
 function randomQuotes(quotes) {
   var used = [];
